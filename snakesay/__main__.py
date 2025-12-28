9e573e979c66bc87229be02eea6875608b8d13f6
PoivronJaune <poivronjaune@gmail.com>
2025-12-27 22:09:27 -0500

    Updated

--- snakesay/__main__.py
+++ snakesay/__main__.py
@@ -1,8 +1,9 @@
 import sys
+
 from snakesay import snake
 
-def default():
+def main():
     snake.say(" ".join(sys.argv[1:]))
 
-if __name__ == '__main__':
-    default()    
+if __name__ == "__main__":
+    main()
--- snakesay/snake.py
+++ snakesay/snake.py
@@ -1,49 +1,21 @@
-SNAKE = r"""  \
+SNAKE = r"""
    \    __
-    \  {oo}    
-       (__) \
-         λ  \\
+    \  {oo}
+       (__)\
+         λ \\
             _\\__
            (_____)_
-          (________)Ooᵒ
-"""
-
-CAT = r"""  \ 
-   \
-     /\_/\ 
-    ( o.o ) 
-     > ^ < 
- """
-
-DOG = r"""  \
-   \
-     /^ ^\ 
-    / 0 0 \ 
-    V\ Y /V
-     / - \ 
-     |    \
-     || (__V
+          (________)Oo°
 """
 
 def bubble(message):
     bubble_length = len(message) + 2
-    return f""" 
+    return f"""
   {"_" * bubble_length}
  ( {message} )
-  {"‾" * bubble_length}"""
-
-def say(message='Hello go to bed'):
-    print(bubble(message))  
-    print(SNAKE)
-
-def hiss(message="Sleep little cub man"):
-    print(bubble(message))  
-    print(SNAKE)
-
-def meow(message="Meow, milk please"):
-    print(bubble(message))  
-    print(CAT)
-
-def bark(message="Hey, trhow the stick"):
-    print(bubble(message))  
-    print(DOG)
+  {"⎻" * bubble_length}"""    
+  
+def say(message):
+    print(bubble(message))
+    print(SNAKE)  
+    
